548a7bf4b7fbd32d8989bb17e0bec2b58bc7a049
MrFlooky <minecatking@gmail.com>
2022-12-29 00:11:17 +0200

    basic drop file

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text.RegularExpressions;
+
+
+while (true) {
+	Console.WriteLine("Hi! Drop a file here: ");
+	string? path = Console.ReadLine();
+	if (pathRegex().IsMatch(path) && File.Exists(path)) {
+		Console.WriteLine("File is a text file.");
+	}
+	else {
+		Console.WriteLine("Drop a text file.");
+	}
+	Console.WriteLine("Do you want to exit? ( y / n ): ");
+	path = Console.ReadLine();
+	if (path == "y" || path == "Y")
+		break;
+	Console.Clear();
+}
+
+partial class Program {
+	[GeneratedRegex(@"^[A-Za-z]:(?:\\[^\\\/:*?""<>\|]+)*\\[^\\\/:*?""<>\|]+\.txt$")]
+	private static partial Regex pathRegex();
+}
